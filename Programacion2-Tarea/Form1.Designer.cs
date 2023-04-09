aca96ac95c73f348d244cdce7d092a68a36a9702
Thomas Agustin Dergazarian <105390864+DergazarianThomas@users.noreply.github.com>
2023-04-09 16:55:03 -0300

    Agregar archivos de proyecto.

---
+++ Programacion2-Tarea/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Programacion2_Tarea
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.txtMes = new System.Windows.Forms.TextBox();
+            this.txtAño = new System.Windows.Forms.TextBox();
+            this.txtDia = new System.Windows.Forms.TextBox();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(23, 38);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(556, 25);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Ingrese una fecha en el siguente formato \"dd, mm, aaaa\"";
+            // 
+            // txtMes
+            // 
+            this.txtMes.Location = new System.Drawing.Point(280, 120);
+            this.txtMes.MaxLength = 2;
+            this.txtMes.Name = "txtMes";
+            this.txtMes.Size = new System.Drawing.Size(44, 20);
+            this.txtMes.TabIndex = 1;
+            this.txtMes.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            // 
+            // txtAño
+            // 
+            this.txtAño.Location = new System.Drawing.Point(355, 120);
+            this.txtAño.MaxLength = 4;
+            this.txtAño.Name = "txtAño";
+            this.txtAño.Size = new System.Drawing.Size(44, 20);
+            this.txtAño.TabIndex = 2;
+            this.txtAño.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            // 
+            // txtDia
+            // 
+            this.txtDia.Location = new System.Drawing.Point(204, 120);
+            this.txtDia.MaxLength = 2;
+            this.txtDia.Name = "txtDia";
+            this.txtDia.Size = new System.Drawing.Size(44, 20);
+            this.txtDia.TabIndex = 3;
+            this.txtDia.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFecha.Location = new System.Drawing.Point(188, 234);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(0, 25);
+            this.lblFecha.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(255, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "/";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(330, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "/";
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvertir.Location = new System.Drawing.Point(265, 170);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(75, 23);
+            this.btnConvertir.TabIndex = 7;
+            this.btnConvertir.Text = "Convertir";
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(603, 450);
+            this.Controls.Add(this.btnConvertir);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.txtDia);
+            this.Controls.Add(this.txtAño);
+            this.Controls.Add(this.txtMes);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "form1";
+            this.Text = "Formato De Fechas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.TextBox txtMes;
+        private System.Windows.Forms.TextBox txtAño;
+        private System.Windows.Forms.TextBox txtDia;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConvertir;
+    }
+}
+
---
+++ Programacion2-Tarea/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programacion2_Tarea
+{
+    public partial class form1 : Form
+    {
+        const string ERROR_FECHA = "La fecha ingresada no es valida";
+       
+        public form1()
+        {
+            
+            InitializeComponent();
+            
+        }
+
+        // Funcion para que solo se admitan numeros //
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((!char.IsNumber(e.KeyChar)) && (!char.IsControl(e.KeyChar)))
+            {
+                e.Handled = true;
+            }
+
+        }
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            int mes = int.Parse(txtMes.Text);
+            int dia = int.Parse(txtDia.Text);
+            int año = int.Parse(txtAño.Text);
+            string mesFinal = "";
+            bool i = true;
+
+            if (mes == 1 && 1 <= dia && dia <= 30)
+            {
+                mesFinal = "Enero";
+                dia = dia + 1;
+            }
+            else if ( mes == 1 && dia == 31)
+            {
+                mesFinal = "Febrero";
+                dia = 1;
+            }
+            else if (mes == 2 && 1 <= dia && dia <= 27)
+            {
+                mesFinal = "Febrero";
+                dia = dia + 1;
+            }
+            else if (mes == 2 && dia == 28)
+            {
+                mesFinal = "Marzo";
+                dia = 1;
+            }
+            else if (mes == 3 && 1 <= dia && dia <= 30)
+            {
+                mesFinal = "Marzo";
+                dia = dia + 1;
+            }
+            else if (mes == 3 && dia == 31)
+            {
+                mesFinal = "Abril";
+                dia = 1;
+            }
+            else if (mes == 4 && 1 <= dia && dia <= 29)
+            {
+                mesFinal = "Abril";
+                dia = dia + 1;
+            }
+            else if (mes == 4 && dia == 30)
+            {
+                mesFinal = "Mayo";
+                dia = 1;
+            }
+            else if (mes == 5 && 1 <= dia && dia <= 30)
+            {
+                mesFinal = "Mayo";
+                dia = dia + 1;
+            }
+            else if (mes == 5 && dia == 31)
+            {
+                mesFinal = "Junio";
+                dia = 1;
+            }
+            else if (mes == 6 && 1 <= dia && dia <= 29)
+            {
+                mesFinal = "Junio";
+                dia = dia + 1;
+            }
+            else if (mes == 6 && dia == 30)
+            {
+                mesFinal = "Julio";
+                dia = 1;
+            }
+            else if (mes == 7 && 1 <= dia && dia <= 30)
+            {
+                mesFinal = "Julio";
+                dia = dia + 1;
+            }
+            else if (mes == 7 && dia == 31)
+            {
+                mesFinal = "Agosto";
+                dia = 1;
+            }
+            else if (mes == 8 && 1 <= dia && dia <= 30)
+            {
+                mesFinal = "Agosto";
+                dia = dia + 1;
+            }
+            else if (mes == 8 && dia == 31)
+            {
+                mesFinal = "Septiembre";
+                dia = 1;
+            }
+            else if (mes == 9 && 1 <= dia && dia <= 29)
+            {
+                mesFinal = "Septiembre";
+                dia = dia + 1;
+            }
+            else if (mes == 9 && dia == 30)
+            {
+                mesFinal = "Octubre";
+                dia = 1;
+            }
+            else if (mes == 10 && 1 <= dia && dia <= 30)
+            {
+                mesFinal = "octubre";
+                dia = dia + 1;
+            }
+            else if (mes == 10 && dia == 31)
+            {
+                mesFinal = "Noviembre";
+                dia = 1;
+            }
+            else if (mes == 11 && 1 <= dia && dia <= 29)
+            {
+                mesFinal = "noviembre";
+                dia = dia + 1;
+            }
+            else if (mes == 11 && dia == 30)
+            {
+                mesFinal = "Diciembre";
+                dia = 1;
+            }
+            else if (mes == 12 && 1 <= dia && dia <= 30)
+            {
+                mesFinal = "diciembre";
+                dia = dia + 1;
+            }
+            else if (mes == 12 && dia == 31)
+            {
+                mesFinal = "Enero";
+                dia = 1;
+                año = año + 1;
+            }
+            else
+            {
+                MessageBox.Show(this, ERROR_FECHA, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                i = false;
+            }
+
+            
+
+            if (i == true)
+            {
+                lblFecha.Text = Convert.ToString(dia) + " de " + mesFinal + " de " + año;
+            } 
+        }
+    }
+}
